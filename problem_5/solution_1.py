3415053637db8cd0954dbb616c68c9d6f7cfaa3e
Rohan George <rohan.geo.2001@gmail.com>
2020-10-18 14:27:53 +0530

    solved problem 5

---
+++ problem_5/solution_1.py
@@ -0,0 +1,8 @@
+def hcf(x,y): return y and hcf(y, x % y) or x
+def lcm(x,y): return x * y / hcf(x,y)
+def solution():
+    n = 1
+    for i in range(1, 31):
+        n = lcm(n, i)
+    print(n)
+solution()
